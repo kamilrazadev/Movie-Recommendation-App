8cb462cfe033092057b8a96cbbb874f00fc313d7
M Kamil Raza <125785123+kamilraza-dev@users.noreply.github.com>
2023-04-17 16:36:48 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,77 +1,54 @@
-// Use Of includes Function
-// const  info = "My Name is Kamil";
-// let result = info.toLowerCase().includes("kamil");
-// console.log(result);
-// ----------------------------END
+(async function() {
+    const fetched = await fetch("./data.json");
+    const movies = await fetched.json();
 
-//IIFE imidiately invoked function expression 
-//run imidiately when complete
-//use to fetch .json file and all code of js do here
+    const selectedLang = document.getElementById("langDropBtn");
+    const selectedRating = document.getElementById("ratingDropBtn");
+    const selectedYear = document.getElementById("yearDropBtn");
+    const searchButton = document.getElementById("searchBtn");
+    const displayElem = document.getElementById("searchedMovies");
+    
 
-(async function () {
-    const fetched = await fetch("./recipes.json");
-    const recipes = await fetched.json();
 
-    const userInput = document.getElementById("search-input");
-    const btnElem = document.getElementById("Search-Btn");
-    const searchList = document.getElementById("Recipe-list");
-    const recipeDetailsContainer = document.getElementById("Recipe-Details-Container");
+    function userSearch(){
+        const langValue = selectedLang.options[selectedLang.selectedIndex].value;
+        const ratingValue = selectedRating.options[selectedRating.selectedIndex].value;
+        const yearValue = selectedYear.value;
+        console.log(yearValue);
+        const searchResults = movies.filter(function (filteredMovie) {
+            return (filteredMovie.original_language.includes(langValue) && 
+                    filteredMovie.vote_average > ratingValue &&
+                    filteredMovie.release_date.substring(0,4).includes(yearValue));
+        }
+        )
+        displaySearchResults(searchResults);
 
-    function recipeDetails(result){
-        recipeDetailsContainer.innerHTML = `
-            <h3 class="margin">${result.title}</h3>
-            <ul class="margin">
-            ${result.ingredients.map(function(ingredients){
-                return "<li>" + ingredients + "</li>"
-            }).join("")}
-            <ul>
-            <h3 class="margin">Description</h3>
-            <p class="margin">
-            ${result.description}
-            </p>   
-            <h3 class="margin">Instructions</h3>
-            <p class="margin">
-            ${result.instructions}
-            </p>
-            <h3 class="margin">Author</h3>
-            <p class="margin">
-            ${result.author}
-            </p>
-            <h3 class="margin">See More Details</h3>
-            <p class="margin">
-            <a href="${result.url}">${result.url}</a>
-            </p>         
-        `;
-        console.log(result);
-    }
+    };
 
     function displaySearchResults(searchResults){
-        searchList.innerHTML = "";
-        searchResults.forEach(
-            function(result){
-                const li = document.createElement("li");
-                li.innerHTML = `<h4>${result.title}<h4>`;
-                
-                searchList.appendChild(li);
-            }
+        let i = 1;
+        displayElem.innerHTML = "";
+
+        searchResults.forEach(function(movie) {
+            const ul = document.createElement("ul"); 
+            ul.className = "movieItem";
+            ul.innerHTML = `
+            <li class="small">${i}</li>
+            <li class="large"><h4 class="movieTitle">${movie.title}</h4><b class="rate">Ratings ${movie.vote_average}</b> | <i>${movie.original_language}</i></li>
+            <li class="small"><i>${movie.release_date.substring(0,4)}</i></li>
+           `
+            i++;
+            displayElem.appendChild(ul);
+        }
         );
+
+
+
+    }
         
-    }
+        
+    
 
-    function searchFunc() {
-        const searchValue =  userInput.value;
-        const searchResults = recipes.filter(function (recipe) {
-            return (recipe.title.toLowerCase().includes(searchValue) || 
-                        recipe.ingredients.join(" ").toLowerCase().includes(searchValue)); // here join() used to make array a string with " " a space btw each element
-        });
-
-        // console.log(searchResults.title);
-        displaySearchResults(searchResults);
-    }
-
-    btnElem.addEventListener("click", searchFunc);
+    searchButton.addEventListener("click", userSearch);
 
 })();
-
-
-
